ca48760a0b456d32e3912aa85763107beeac6092
fan-dc <fan.zhou@daocloud.io>
2019-08-15 15:07:24 +0800

    helloworld代码

---
+++ src/main/java/io/daocloud/cr/hello/HelloApplication.java
@@ -0,0 +1,23 @@
+/**
+ * 2019 fan.z
+ */
+package io.daocloud.cr.hello;
+
+/**
+ * <pre>
+ * 类说明：
+ * 
+ * Modify Information:
+ * Author         Date         Description
+ * ============ =========== ============================
+ * fan.z        2019-08-15    Create this file
+ * 
+ * </pre>
+ * 
+ */
+public class HelloApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
